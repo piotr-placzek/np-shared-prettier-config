b8f2ac2c019d4149e734bab4e9b4edbce17bf085
Piotr Płaczek <piotr.placzek.91@gmail.com>
2022-09-26 18:05:57 +0200

    init gh package

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+// https://prettier.io/docs/en/options.html
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: false,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    bracketSameLine: true,
+    arrowParens: 'always',
+    requirePragma: false,
+    insertPragma: false,
+    endOfLine: 'auto',
+};
